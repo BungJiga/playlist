4cb34b74313a407288b81d29fc85ae43cdf1fc1b
Michael Guerrero <michaelguerrero@uagateway.org>
2019-01-14 22:46:39 +0000

    Adding more

--- js/script.js
+++ js/script.js
@@ -3,8 +3,8 @@
 // BELOW Update the songs array with four of your favorites songs.
 var songs = ["Not Irl","Jungle Fury" , "Walking On The Moon", "Fight"  ];
 var image_links = ["https://pbs.twimg.com/media/DwgEzEaVAAApULc.jpg"];
-var artists = ["Modestep" , ];
-var lengths = ["4:12" ];
+var artists = ["Modestep","RIOT","Infected Mushroom","Tokyo Machine" ];
+var lengths = ["4:12"];
 var links = ["https://www.youtube.com/watch?v=SzQKBrU1YWM"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
@@ -13,6 +13,29 @@ var links = ["https://www.youtube.com/watch?v=SzQKBrU1YWM"];
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
+for (var i = 0; i < songs.length; i++){
+    
+ $("#songs").append(songs[i]);
+    
+    
+}
+for (var i = 0; i < artists.length; i++){
+    
+ $("#artists").append(artists[i]);
+    
+    
+}
+for (var i = 0; i < image_links.length; i++){
+    
+console.log(image_links[i]);
+  $("#images").html('<img src=" ' + image_links[i] + '">' );  
+    
+}
+for (var i = 0; i < lengths.length; i++){
+    
+ $("#lengths").append(lengths[i]);
+}
+
 
 }
 
