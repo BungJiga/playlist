83efcfc92a6e1596d612bc54949811553f8b1cbf
Michael Guerrero <michaelguerrero@uagateway.org>
2019-01-28 23:05:33 +0000

    adding

--- js/script.js
+++ js/script.js
@@ -2,10 +2,10 @@
 
 // BELOW Update the songs array with four of your favorites songs.
 var songs = ["Not Irl","Jungle Fury" , "Walking On The Moon", "Fight"  ];
-var image_links = ["https://pbs.twimg.com/media/DwgEzEaVAAApULc.jpg"];
+var image_links = ["https://pbs.twimg.com/media/DwgEzEaVAAApULc.jpg" , "https://f4.bcbits.com/img/a4272051150_10.jpg" , "https://images.genius.com/2f97138abde711240af43d0e5d015d13.1000x1000x1.jpg", "https://vignette.wikia.nocookie.net/monstercat/images/4/40/Tokyo_Machine_-_FIGHT.jpg/revision/latest?cb=20161021172627"];
 var artists = ["Modestep","RIOT","Infected Mushroom","Tokyo Machine" ];
-var lengths = ["4:12"];
-var links = ["https://www.youtube.com/watch?v=SzQKBrU1YWM" , "test"];
+var lengths = ["4:12","5:11","5:36","3:36"];
+var links = ["https://www.youtube.com/watch?v=SzQKBrU1YWM" , "https://www.youtube.com/watch?v=mype6RcMMmY" , "https://www.youtube.com/watch?v=U4hMQtcKfQo" , "https://www.youtube.com/watch?v=qvBlHBHUTMs"];
 // BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
 // Make sure they match the same order as your array above
 
@@ -13,33 +13,42 @@ var links = ["https://www.youtube.com/watch?v=SzQKBrU1YWM" , "test"];
 
 function displaySongInfo(){
     // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
+  $("#songs").append('<p  class="one">' + "Songs" + '</p>');
 for (var i = 0; i < songs.length; i++){
-    
+
  $("#songs").append('<p>' + songs[i] + '</p>');
     
     
 }
+ $("#artists").append('<p  class="one">' + "Artists" + '</p>');   
 for (var i = 0; i < artists.length; i++){
-    
+
  $("#artists").append('<p>' + artists[i] + '</p>');
     
     
 }
+   $("#images").append('<p  class="one">' + "Images" + '</p>'); 
 for (var i = 0; i < image_links.length; i++){
     
-console.log(image_links[i]);
-  $("#images").html('<img src=" ' + image_links[i] + '">' );  
+  
+  $("#images").append('<img src=" ' + image_links[i] + '">' );  
     
 }
+ $("#lengths").append('<p  class="one">' + "Length" + '</p>'); 
 for (var i = 0; i < lengths.length; i++){
-    
+ 
+
  $("#lengths").append('<p>' + lengths[i] + '</p>');
 }
 
-for (var i = 0; i < links.length; i++){
-    
- $("#links").append('<a  href="' + links[i] + '" target="_blank">' + 'Listen' + '</a>');
-}
+ $("#links").append('<p  class="one">' + "Link" + '</p>'); 
+
+links.forEach(function(link){
+ 
+ $("#links").append('<a  href="' + link + '" target="_blank">' + 'Listen' + '</a>');
+ 
+ 
+});
 
 
 }
@@ -50,9 +59,6 @@ function emptySongInfo(){
     $("#images").empty();
     $("#lengths").empty();
     $("#links").empty();
-    // Use jQuery to empty all of the remaining divs
-
-
 }
 
 
